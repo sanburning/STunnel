adf9ffd6477035c25fdd4a3b77c9c9fe602f5e4f
sanburning <67701804+sanburning@users.noreply.github.com>
2025-12-28 22:43:06 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/stunnel/MainActivity.java
+++ app/src/main/java/com/example/stunnel/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.stunnel;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
